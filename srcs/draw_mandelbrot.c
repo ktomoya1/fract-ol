c39ff5421a59eb79b6771324c2ee2d2857e1f9f3
ktomoya <twbtomoya2@gmail.com>
2023-08-07 19:24:38 +0900

    delete comment

--- srcs/draw_mandelbrot.c
+++ srcs/draw_mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 18:15:53 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/08/07 16:55:49 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/08/07 19:02:09 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,6 @@ void	draw_mandelbrot(t_data *img)
 	int		y;
 	int		i;
 
-	// 1.描画する領域の範囲を指定する
 	y = 0;
 	img->x_min = -2.0;
 	img->x_max = 2.0;
@@ -41,17 +40,12 @@ void	draw_mandelbrot(t_data *img)
 	while (y < img->height)
 	{
 		x = 0;
-		// 2. 各点に対して複素数cを割り当てる
-		// 画像の幅と高さを使用して、各点に対応する複素数cを計算する
 		while (x < img->width)
 		{
 			c.re = img->x_min + x * (img->x_max - img->x_min) / img->width;
 			c.im = img->y_min + y * (img->y_max - img->y_min) / img->height;
-			// 3. zを0に設定する
 			z.re = 0;
 			z.im = 0;
-			// 4. zがマンデルブロ集合に属するかどうかを判定する
-			// z = z^2 + c を繰り返し適用し、zの絶対値が2を超えたら、その点はマンデルブロ集合に属さないと判定する
 			i = 0;
 			while (i < MAX_ITERATIONS)
 			{
@@ -60,11 +54,10 @@ void	draw_mandelbrot(t_data *img)
 					break ;
 				i++;
 			}
-			// 5.zがマンデルブロ集合に属する場合は、黒色、属さない場合は白色で点を描画する
 			if (i == MAX_ITERATIONS)
 				my_mlx_pixel_put(img, x, y, 0x000000);
 			else
-				my_mlx_pixel_put(img, x, y, 0xFFFFFF);
+				my_mlx_pixel_put(img, x, y, 0x0000FF);
 			x++;
 		}
 		y++;
