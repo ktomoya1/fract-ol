04da638add932103f875ca85a8af7cc982f66f59
ktomoya <twbtomoya2@gmail.com>
2023-08-10 19:37:48 +0900

    same up

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 16:38:58 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/08/10 18:27:07 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/08/10 19:08:08 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,25 +30,34 @@ int	esc_close(int keycode, t_vars *data)
 	return (0);
 }
 
-int exit_on_close(t_vars *data)
+int	exit_on_close(t_vars *data)
 {
 	mlx_destroy_window(data->mlx, data->win);
 	exit(0);
 }
 
-int	mouse_scroll(int button, int x, int y, t_vars *data)
-{
-	(void)data;
-	if (button == 4)
-	{
-		printf("Scrolling up | x: %d | y: %d\n", x, y);
-	}
-	else if (button == 5)
-	{
-		printf("Scrolling down | x: %d | y: %d\n", x, y);
-	}
-	return (0);
-}
+// int	mouse_scroll(int button, int x, int y, t_vars *data)
+// {
+// 	double	center_x;
+// 	double	center_y;
+
+// 	(void)x;
+// 	(void)y;
+// 	// 拡大率を調整する
+// 	data->zoom *= 1.1;
+
+// 	// 中心座標を計算する
+// 	center_x = 
+// 	if (button == 4)
+// 	{
+// 		printf("Scrolling up | x: %d | y: %d\n", x, y);
+// 	}
+// 	else if (button == 5)
+// 	{
+// 		printf("Scrolling down | x: %d | y: %d\n", x, y);
+// 	}
+// 	return (0);
+// }
 
 // int	key_press(int keycode, t_vars *param)
 // {
@@ -63,25 +72,23 @@ int	mouse_scroll(int button, int x, int y, t_vars *data)
 int	main(void)
 {
 	t_vars	data;
-	t_range	range;
 
-	range.x_min = -2.0;
-	range.x_max = 2.0;
-	range.y_min = -2.0;
-	range.y_max = 2.0;
+	data.x_min = -2.0;
+	data.x_max = 2.0;
+	data.y_min = -2.0;
+	data.y_max = 2.0;
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "fractol");
 	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length,
 			&data.endian);
 	// draw_mandelbrot(&data, range);
-	draw_julia(&data, range);
+	draw_julia(&data);
 	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
 	// mlx_mouse_hook(data.win, mouse_scroll, &data);
 	// mlx_mouse_move(data.win, mouse_move, &data);
-	mlx_hook(data.win, 4, 0, mouse_scroll, &data);
+	// mlx_hook(data.win, 4, 0, mouse_scroll, &data);
 	mlx_key_hook(data.win, esc_close, &data);
 	mlx_hook(data.win, 17, 0, exit_on_close, &data);
-	// mlx_hook(mlx_win, 33, 1L << 0, close_window(mlx_key_hook(mlx_win, key_press, NULL), NULL);
 	mlx_loop(data.mlx);
 }
