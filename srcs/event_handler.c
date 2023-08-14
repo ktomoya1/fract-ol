cd6ba42eeb227baf8e904dd2cf07493ab0549160
ktomoya <twbtomoya2@gmail.com>
2023-08-14 18:25:46 +0900

    a

--- srcs/event_handler.c
+++ srcs/event_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   event_handler.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 09:54:54 by kudoutomoya       #+#    #+#             */
-/*   Updated: 2023/08/13 10:25:13 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/08/14 18:19:25 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,27 +38,22 @@ int	exit_on_close(t_vars *data)
 
 int	mouse_scroll(int button, int x, int y, t_vars *data)
 {
-	double	center_x;
-	double	center_y;
-
 	(void)x;
 	(void)y;
-	center_x = 0.0;
-	center_y = 0.0;
 	if (button == MOUSE_SCROLL_UP)
 	{
-		data->x_min = center_x - (center_x - data->x_min) * 1.1;
-		data->x_max = center_x + (data->x_max - center_x) * 1.1;
-		data->y_min = center_y - (center_y - data->y_min) * 1.1;
-		data->y_max = center_y + (data->y_max - center_y) * 1.1;
+		data->x_min = data->x_min * 1.1;
+		data->x_max = data->x_max * 1.1;
+		data->y_min = data->y_min * 1.1;
+		data->y_max = data->y_max * 1.1;
 		draw_fractal(data);
 	}
 	else if (button == MOUSE_SCROLL_DOWN)
 	{
-		data->x_min = center_x - (center_x - data->x_min) / 1.1;
-		data->x_max = center_x + (data->x_max - center_x) / 1.1;
-		data->y_min = center_y - (center_y - data->y_min) / 1.1;
-		data->y_max = center_y + (data->y_max - center_y) / 1.1;
+		data->x_min = data->x_min / 1.1;
+		data->x_max = data->x_max / 1.1;
+		data->y_min = data->y_min / 1.1;
+		data->y_max = data->y_max / 1.1;
 		draw_fractal(data);
 	}
 	return (0);
